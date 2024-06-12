3c3e416bddb4098ea33a1c9779a9dde38962fd6f
James Jeffrey <james.jeffrey1@ftr.com>
2024-06-12 12:55:39 -0700

    and the status

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,3 @@
 alias gtags='git describe --tags | cut -f 1-2 -d "-" '
-alias gntag='git fetch --all;git checkout development;git pull; git tag `git describe --tags | cut -f 1 -d "-"| xargs -n 1 svt -mode dev v0.0.1 `;git push --tag'
-alias gutag='git fetch --all;git checkout staging;git pull; git tag `git tags  | grep uat | sort | tail -n 1 | xargs -n 1 svt -mode uat `;git push --tag'
+alias gntag='git fetch --all;git checkout development;git pull; git tag `git describe --tags --match v*.*.* | cut -f 1 -d "-"| xargs -n 1 svt -mode dev v0.0.1 ` && git push --tag'
+alias gutag='git fetch --all;git checkout staging;git pull; git tag `git tags  | grep uat | sort | tail -n 1 | xargs -n 1 svt -mode uat ` && git push --tag'
