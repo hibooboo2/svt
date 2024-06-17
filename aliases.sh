73b2ab71c7aac267a2b09debaaa9abe7938dec22
James Jeffrey <james.jeffrey1@ftr.com>
2024-06-17 15:50:42 -0700

    grr

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
 # Source this file
 alias gtags='git describe --tags | cut -f 1-2 -d "-" '
-alias gntag='git fetch --all --tags;git checkout development;git pull; git tag `git describe --tags --match v*.*.* | cut -f 1 -d "-"| xargs -n 1 svt -mode dev v0.0.1 ` && git push --tag'
-alias gutag='git fetch --all --tags;git checkout staging;git pull; git tag `git describe --tags --match uat | xargs -n 1 svt -mode uat ` && git push --tag'
+alias gntag='git fetch --all --tags;git checkout development;git pull; git tag `git describe --tags --match v*.*.*- | cut -f 1 -d "-"| xargs -n 1 svt -mode dev v0.0.1 ` && git push --tag'
+alias gutag='git fetch --all --tags;git checkout staging;git pull; git tag `git tags  | grep uat | sort | tail -n 1 | xargs -n 1 svt -mode uat ` && git push --tag'
