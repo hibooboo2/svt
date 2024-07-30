6ce40660b407c20c5060fe51569815707de7d51a
James Jeffrey <james.jeffrey1@ftr.com>
2024-07-30 10:05:28 -0700

    feat: add prod tags

---
+++ prod.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type PROD string
+
+func (u PROD) Version(_ ...Version) Version {
+	parts := strings.Split(string(u), "-")
+	if len(parts) != 2 {
+		return NewProdTag(1)
+	}
+	if parts[0] != "r" {
+		return NewProdTag(1)
+	}
+	parts = strings.Split(parts[1], ".")
+	if len(parts) != 2 {
+		return NewProdTag(1)
+	}
+	if parts[0] == dateStringUATFormat(time.Now()) {
+		start, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return NewProdTag(1)
+		}
+		return NewProdTag(1 + start)
+	}
+	return NewProdTag(1)
+}
+
+func NewProdTag(num int) PROD {
+	return PROD(fmt.Sprintf("r-%s.%d", dateStringUATFormat(time.Now()), num))
+}
