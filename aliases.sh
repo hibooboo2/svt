94f0f24d3f9cee91cd585c292d225338d6ad1445
jjj932 <james.jeffrey1@ftr.com>
2024-10-30 12:31:57 -0700

    stuff probs not working

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 # Source this file
 alias gtags='git describe --tags | cut -f 1-2 -d "-" '
-alias gntag='git fetch --all --tags;git checkout development;git pull; git tag `git tag --points-at HEAD | cut -f 1 -d "-"| xargs -n 1 svt -mode dev v0.0.1 ` && git push --tag'
-alias gutag='git fetch --all --tags;git checkout staging;git pull; git tag `git tags  | grep 'uat-' | sort | tail -n 1 | xargs -n 1 svt -mode uat ` && git push --tag'
-alias gptag='git fetch --all --tags;git checkout production;git pull; git tag `git tags  | grep 'r' | sort | tail -n 1 | xargs -n 1 svt -mode prod ` && git push --tag'
+alias gntag='git fetch --all --tags;git checkout development;git pull; git tag `git describe --tags --match v*.*.* | svt -last-tag -mode dev | cut -f 1 -d "-"| xargs -n 1 svt -mode dev v0.0.1 ` && git push --tag'
+alias gutag='git fetch --all --tags;git checkout staging;git pull; git tag `git describe --tags --match uat-* | svt -last-tag -mode uat | cut -f 1-2 -d "-"| xargs -n 1 svt -mode uat ` && git push --tag'
+alias gptag='git fetch --all --tags;git checkout production;git pull; git tag `git describe --tags --match r* | svt -last-tag -mode prod | cut -f 1 -d "-"| xargs -n 1 svt -mode prod ` && git push --tag'
--- main.go
+++ main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	mode := flag.String("mode", "dev", "mode is what kind of version system you want to increment valid choices are ['dev','uat']")
+	lastTag := flag.Bool("last-tag", false, "the last tag")
+	mode := flag.String("mode", "dev", "mode is what kind of version system you want to increment valid choices are ['dev','uat','prod']")
 	flag.Parse()
 
 	args := flag.Args()
+
+	if *lastTag {
+		lines := bufio.NewReader(os.Stdin)
+		//list out the tags and their versions
+		// convert to ssemver based on type
+		// get the latest version
+		// print the latest version
+		versions := []Version{}
+		for {
+			line, err := lines.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				panic(err)
+			}
+
+			var v Version
+			switch *mode {
+			case "dev":
+				v = SemVer(line)
+			case "uat":
+				v = UAT(line)
+			case "prod":
+				v = PROD(line)
+			default:
+				panic("Invalid mode")
+			}
+			versions = append(versions, v)
+		}
+
+		return
+	}
 
 	switch len(args) {
 	case 1, 2:
