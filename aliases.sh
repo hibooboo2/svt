44b25ffc05fe3469d8d0fdf91c70ca645d94e968
jjj932 <james.jeffrey1@ftr.com>
2025-04-25 08:50:33 -0700

    feat: better tagging

--- aliases.sh
+++ aliases.sh
@@ -7,6 +7,13 @@ pull(){
 
 # Source this file
 tagIfNoTag(){
+    CURRENT_BRANCH=$(git branch --show-current)
+
+    if [[ "$CURRENT_BRANCH" != "$2" ]]; then
+        echo "Error: Not on the '$2' branch."
+        return
+    fi
+
     if [ "$(git tag --points-at HEAD | wc -l)" -eq 0 ]; then
         echo "No tags found on the current commit tagging with: $1"
         git tag $1
@@ -18,21 +25,35 @@ tagIfNoTag(){
 
 alias gtags='git describe --tags | cut -f 1-2 -d "-" '
 
-gntag(){
+ntag(){
     pull development
     TAGNAME=$(git describe --tags --match v*.*.* | cut -f 1 -d "-"| xargs -n 1 svt -mode dev v0.0.1)
     echo $TAGNAME
-    tagIfNoTag $TAGNAME $1
 }
-gutag(){
+
+gntag(){
+    ntag
+    tagIfNoTag $TAGNAME development $1
+}
+
+utag(){
     pull staging
     TAGNAME=$(git describe --tags --match uat-* | cut -f 1-2 -d "-"| xargs -n 1 svt -mode uat)
     echo $TAGNAME
-    tagIfNoTag $TAGNAME $1
 }
-gptag(){
+
+gutag(){
+    utag
+    tagIfNoTag $TAGNAME staging $1
+}
+
+ptag(){
     pull production
     TAGNAME=$(git describe --tags --match r*.* | cut -f 1 -d "-"| xargs -n 1 svt -mode prod)
     echo $TAGNAME
-    tagIfNoTag $TAGNAME $1
+}
+
+gptag(){
+    ptag
+    tagIfNoTag $TAGNAME production $1
 }
